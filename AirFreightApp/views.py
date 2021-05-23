ef909409b41df7a20120744fb4c6eba2519983b8
Mohaymen Sameen <42969169+MohaymenSameen@users.noreply.github.com>
2021-05-23 18:53:11 +0200

    Implemented Table View
    Data from the database can be viewed in the tables for each app (air, ocean and ground)

--- AirFreightApp/views.py
+++ AirFreightApp/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django_filters.views import FilterView
+from django_tables2 import SingleTableMixin
+from .models import AirFreightShipment
+from .air_table import AirTable
+from .air_shipment_filter import AirShipmentFilter
 
 
-# Create your views here.
-def air_freight_dashboard_view(request, *args, **kwargs):
-    return render(request, "AirFreightTemplates/air_freight_dashboard.html")
+class AirListView(SingleTableMixin, FilterView):
+    model = AirFreightShipment
+    table_class = AirTable
+    template_name = "AirFreightTemplates/air_freight_dashboard.html"
+    filterset_class = AirShipmentFilter
+
+
+
--- damen_django_project/urls.py
+++ damen_django_project/urls.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from DamenShipyardsApp.views import index_home_view
-from OceanFreightApp.views import shipment_detail_view
-from OceanFreightApp.views import index_dashboard_view
-from AirFreightApp.views import air_freight_dashboard_view
-from GroundShippingApp.views import ground_shipping_dashboard_view
+from OceanFreightApp.views import OceanListView
+from AirFreightApp.views import AirListView
+from GroundShippingApp.views import GroundListView
 urlpatterns = [
     path('', index_home_view, name='home'),
-    path('dashboard/ocean', index_dashboard_view, name='ocean_dashboard'),
-    path('OceanFreightApp', shipment_detail_view, name='OceanFreightApp'),
-    path('dashboard/air', air_freight_dashboard_view, name='air_dashboard'),
-    path('dashboard/ground', ground_shipping_dashboard_view, name='ground_dashboard'),
+    path('dashboard/ocean', OceanListView.as_view()),
+    path('dashboard/air', AirListView.as_view()),
+    path('dashboard/ground', GroundListView.as_view()),
     path('admin/', admin.site.urls)
 ]
