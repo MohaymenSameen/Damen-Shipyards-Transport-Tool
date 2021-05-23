58306d136b207973043f840d16a18471d4f960e0
Mohaymen Sameen <42969169+MohaymenSameen@users.noreply.github.com>
2021-05-23 15:55:09 +0200

    Template Rendering & Inheritance
    Using inheritance for the dashboard to reduce repeated code. Also added new templates for the air, ground and ocean apps

--- AirFreightApp/views.py
+++ AirFreightApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def air_freight_dashboard_view(request, *args, **kwargs):
+    return render(request, "AirFreightTemplates/air_freight_dashboard.html")
