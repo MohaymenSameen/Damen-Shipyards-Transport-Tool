0d02fafcacb863a7bc10983d6ee66ee5f9628ebf
Mohaymen Sameen <42969169+MohaymenSameen@users.noreply.github.com>
2021-06-08 15:48:31 +0200

    Uploading Schenker Dataset
    You can now upload a schenker dataset and it will be added to the database

--- OceanFreightApp/views.py
+++ OceanFreightApp/views.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from django.shortcuts import render
 from django_tables2 import SingleTableMixin
 from django_filters.views import FilterView
 from rest_framework.views import APIView
@@ -5,6 +7,7 @@ from rest_framework.response import Response
 from .ocean_table import OceanTable
 from .models import OceanFreightShipment
 from .ocean_shipment_filter import OceanShipmentFilter
+from django.core.files.storage import FileSystemStorage
 
 
 class OceanListView(SingleTableMixin, FilterView):
@@ -12,6 +15,55 @@ class OceanListView(SingleTableMixin, FilterView):
     table_class = OceanTable
     template_name = 'OceanFreightTemplates/ocean_freight_dashboard.html'
     filterset_class = OceanShipmentFilter
+
+
+def upload_file(request):
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+
+        df = pd.read_excel(request.FILES.get('document'))
+
+        df['ETD Date (GMT Time)'] = pd.to_datetime(df['ETD Date (GMT Time)'], format='%d/%M/%Y %H%M').dt.strftime(
+            '%Y-%m-%d %H:%M')
+        df['ETA Date (GMT Time)'] = pd.to_datetime(df['ETA Date (GMT Time)'], format='%d/%M/%Y %H%M',
+                                                   errors='coerce').dt.strftime('%Y-%m-%d %H:%M')
+        df['CTA Date (GMT Time)'] = pd.to_datetime(df['CTA Date (GMT Time)'], format='%d/%M/%Y %H%M',
+                                                   errors='coerce').dt.strftime('%Y-%m-%d %H:%M')
+        df["MainShipmentId"] = df["STT"].astype(str) + df["Container No"].astype(str)
+        df["MainShipmentId"] = df["MainShipmentId"].str.replace(' ', '')
+
+        df['ETD Date (GMT Time)'] = df["ETD Date (GMT Time)"].fillna("2000-01-01")
+        df['ETA Date (GMT Time)'] = df["ETA Date (GMT Time)"].fillna("2000-01-01")
+        df['CTA Date (GMT Time)'] = df["CTA Date (GMT Time)"].fillna("2000-01-01")
+
+        df["STT"] = df["STT"].fillna("")
+        df["Container Weight"] = df["Container Weight"].fillna(0)
+        df["Container Volume"] = df["Container Volume"].fillna(0.0)
+
+        for index, row in df.iterrows():
+            ocean_freight_shipment = OceanFreightShipment()
+
+            ocean_freight_shipment.MainShipmentId = row["MainShipmentId"]
+            ocean_freight_shipment.ShipmentId = pd.to_numeric(row["STT"])
+            ocean_freight_shipment.ContainerNr = row["Container No"]
+            ocean_freight_shipment.Departure = row["Departure Port"]
+            ocean_freight_shipment.Destination = row["Destination Port"]
+            ocean_freight_shipment.ScheduledDeparture = row["ETD Date (GMT Time)"]
+            ocean_freight_shipment.ScheduledArrival = row["ETA Date (GMT Time)"]
+            ocean_freight_shipment.RevisedArrival = row["CTA Date (GMT Time)"]
+            ocean_freight_shipment.TotalVolume = pd.to_numeric(row["Container Volume"])
+            ocean_freight_shipment.TotalWeight = pd.to_numeric(row["Container Weight"])
+            ocean_freight_shipment.Dimensions = row['Container Movement']
+            ocean_freight_shipment.Carrier = row["Carrier"]
+            ocean_freight_shipment.ShipperReference = row["Shipper Reference"]
+
+            ocean_freight_shipment.save()
+
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(uploaded_file)
+    return render(request, 'OceanFreightTemplates/upload.html', context)
 
 
 class OceanChartData(APIView):
--- damen_django_project/urls.py
+++ damen_django_project/urls.py
@@ -1,20 +1,29 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from DamenShipyardsApp.views import index_home_view, login_view, logout_view
-from OceanFreightApp.views import OceanListView, OceanChartData
+from OceanFreightApp.views import OceanListView, OceanChartData, upload_file
 from AirFreightApp.views import AirListView, AirChartData
 from GroundShippingApp.views import GroundListView, ChartData
 from django.contrib.auth.decorators import login_required
+
+from damen_django_project import settings
 
 urlpatterns = [
     path('', index_home_view, name='home'),
     path('login', login_view, name='login'),
     path('logout', logout_view, name='logout'),
-    path('dashboard/ocean', login_required(OceanListView.as_view())),
-    path('dashboard/air', login_required(AirListView.as_view())),
-    path('dashboard/ground', login_required(GroundListView.as_view())),
+    path('dashboard/ocean', login_required(OceanListView.as_view()), name='oceandashboard'),
+    path('dashboard/ocean/upload', login_required(upload_file), name='uploadoceanfile'),
+    path('dashboard/air', login_required(AirListView.as_view()), name='airdashboard'),
+    path('dashboard/ground', login_required(GroundListView.as_view()), name='grounddashboard'),
     path('dashboard/ground/api/chart/data', login_required(ChartData.as_view())),
     path('dashboard/air/api/chart/data', login_required(AirChartData.as_view())),
     path('dashboard/ocean/api/chart/data', login_required(OceanChartData.as_view())),
     path('admin/', admin.site.urls)
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
