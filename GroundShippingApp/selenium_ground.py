24a2743864469784135c66b71f30fb990764f0c6
prinsalvino <42965896+prinsalvino@users.noreply.github.com>
2021-06-08 19:13:50 +0200

    Update selenium_ground.py

--- GroundShippingApp/selenium_ground.py
+++ GroundShippingApp/selenium_ground.py
@@ -28,10 +28,34 @@ selectColumns = selectColumns.to_numpy()
 
 def getDataDHL(column):
     if driver.find_element_by_xpath("//h1[@class='l-grid--w-100pc-s']").text == 'TRACK: PARCEL':
+        # ETA
         column[22] = driver.find_element_by_xpath("//div[@class='c-tracking-result--status-copy-date  ']").text
         listInfo = driver.find_elements_by_xpath(
             "//h4[@class='c-tracking-result--checkpoint--date  l-grid--w-100pc-s']")
+        # ETC
         column[21] = listInfo[-1].text
+
+
+def getDbSchenker(column):
+    ETD = driver.find_element_by_xpath("//span[@data-test='scheduled_departure_value']").text
+    ETA = driver.find_element_by_xpath("//span[@data-test='scheduled_arrival_value']").text
+    revisedArrival = driver.find_element_by_xpath("//span[@data-test='revised_arrival_value']").text
+
+    driver.find_element_by_xpath("//h5[@data-test='shipment_status_history_label']").click()
+    status = driver.find_elements_by_xpath("//td[@data-label='Event']")
+    status = status[-1].text
+    column[1] = status
+
+    if ETD == "-" or ETA == "-":
+        return
+    # New Format Date Time
+    ETD = datetime.strptime(ETD, '%Y/%m/%d %H:%M').strftime("%Y-%m-%d")
+    ETA = datetime.strptime(ETA, '%Y/%m/%d %H:%M').strftime("%Y-%m-%d")
+    column[21] = ETD
+    column[22] = ETA
+    if revisedArrival != "-":
+        revisedArrival = datetime.strptime(revisedArrival, '%Y/%m/%d %H:%M').strftime("%Y-%m-%d")
+        column[22] = revisedArrival
 
 
 for column in selectColumns:
@@ -47,6 +71,46 @@ for column in selectColumns:
                 getDataDHL(column)
             except:
                 continue
+    elif column[20] == "DB Schenker":
+        driver.get("https://eschenker.dbschenker.com/app/tracking-public/?refNumber=" + str(column[2]))
+        try:
+            iframe = driver.find_element_by_tag_name('iframe')
+            driver.switch_to.frame(iframe)
+            driver.find_element_by_xpath(
+                "//button[@class='mat-focus-indicator primary mat-button mat-button-base']").click()
+            getDbSchenker(column)
+        except:
+            try:
+                getDbSchenker(column)
+            except:
+                continue
+    elif column[20] == "Fedex":
+        # Pending 784098044813
+        driver.get("https://www.fedex.com/fedextrack/?action=track&trackingnumber=" + str(column[2]))
+        try:
+            status = driver.find_element_by_xpath("//h3[@data-test-id='key-status']").text
+            # driver.find_element_by_xpath("//span[@data-test-id='delivery-date-text']").text
+            if status == "Pending":
+                column[1] = status
+                continue
+            else:
+                driver.find_element_by_xpath("//button[@id='tab-list-item-tab_panel_3']").click()
+                xlist = driver.find_elements_by_xpath("//div[@class='key-value-list__value color-gray-5']")
+                ETD = xlist[4].text
+                ETA = xlist[5].text
+                ETA = ETA.replace('at ','')
+                ETA = ETA.replace('21','2021')
+
+                ETA = datetime.strptime(ETA, '%m/%d/%Y %I:%M %p').strftime("%Y-%m-%d %H:%M")
+                ETD = ETD.replace(' Ship Date Tooltip','')
+                ETD = ETD.replace('21','2021')
+                ETD = datetime.strptime(ETD, '%m/%d/%Y').strftime("%Y-%m-%d %H:%M")
+                column[21] = ETD
+                column[22] = ETA
+        except:
+            continue
+
+
 
 driver.close()
 
